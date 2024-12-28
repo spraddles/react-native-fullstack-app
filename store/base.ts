b8e95506374d7422fe8038460584b0b5d6d0e5d8
Brett Spradbury <brett.spradbury@gmail.com>
2024-12-28 16:15:54 -0300

    lint fixes

--- store/base.ts
+++ store/base.ts
@@ -1,40 +1,39 @@
 import { create } from 'zustand'
 
 type State = {
-    user: {
-        name: string;
-        surname: string;
-        email: string;
-        phone: string;
-        cpf: string;
-    }
-    loading: boolean;
-    setUser: (field: keyof State['user'], value: string) => void;
-    setLoading: (value: boolean) => void;
+	user: {
+		name: string
+		surname: string
+		email: string
+		phone: string
+		cpf: string
+	}
+	loading: boolean
+	setUser: (field: keyof State['user'], value: string) => void
+	setLoading: (value: boolean) => void
 }
 
-export const useBaseStore = create < State > ()((set) => ({
+export const useBaseStore = create<State>()((set) => ({
+	user: {
+		name: '',
+		surname: '',
+		email: '',
+		phone: '',
+		cpf: ''
+	},
 
-    user: {
-        name: '',
-        surname: '',
-        email: '',
-        phone: '',
-        cpf: ''
-    },
+	loading: false,
 
-    loading: false,
-    
-    setUser: (field, value) =>
-        set((state) => ({
-            user: {
-                ...state.user,
-                [field]: value
-            }
-        })),
-        
-    setLoading: (value) =>
-        set(() => ({
-            loading: value
-        }))
+	setUser: (field, value) =>
+		set((state) => ({
+			user: {
+				...state.user,
+				[field]: value
+			}
+		})),
+
+	setLoading: (value) =>
+		set(() => ({
+			loading: value
+		}))
 }))
