afc1360bf3af112fb586141b6ec69687e0d583b6
Brett Spradbury <brett.spradbury@gmail.com>
2024-12-29 11:53:13 -0300

    fix seeder
    was a type issue

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,6 @@ import { ExpoConfig, ConfigContext } from 'expo/config'
 export default ({ config }: ConfigContext): ExpoConfig => ({
 	...config,
 	extra: {
-		seedData: process.env.SEED_DATA
+		seedData: process.env.SEED_DATA === 'true'
 	}
 })
